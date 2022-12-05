a7fe60062cd45db1c803e30d59039e3b0699c276
Lelikoth <wmatu227@gmail.com>
2022-12-05 07:32:55 +0100

    day5

---
+++ 5_dec/day5_part1.py
@@ -0,0 +1,46 @@
+# program to find the final position of the crates
+
+with open('5_dec/input.txt') as f:
+    lines = f.readlines()
+
+# create a list of lists
+stacks = []
+
+for i in range(9):
+    stacks.append([])
+
+# add the crates to the stacks
+for j in range(8):
+    for i in range(9):
+        stacks[i].append(lines[j][i*4 + 1])
+
+
+#remove all ' ' from the lists
+for i in range(9):
+    stacks[i] = [x for x in stacks[i] if x != ' ']
+
+
+
+#reverse the stacks
+for i in range(9):
+    stacks[i].reverse()
+
+stacks2 = stacks.deepcopy()
+
+
+# move parameters
+for line in lines[10:]:
+    line = line.split()
+    how_many = int(line[1])
+    from_stack = int(line[3])-1
+    to_stack = int(line[5])-1
+
+#move the crates day
+    for i in range(how_many):
+        if len(stacks[from_stack]) > 0:
+            stacks[to_stack].append(stacks[from_stack].pop())
+            
+#get last crate in each stack
+
+for i in range(9):
+    print(stacks[i][-1], end='')
